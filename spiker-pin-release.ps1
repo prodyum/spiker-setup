5a6abd6fb4c3b8604166de86cf5156c84ac031ac
Hasan Ozdemir <hasanozdemir@live.com>
2026-06-12 03:24:03 +0300

    Remove deprecated release pin shims

--- spiker-pin-release.ps1
+++
@@ -1,6 +0,0 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
-
-Write-Warning 'spiker-pin-release.ps1 is deprecated. Use pin-release-version.ps1.'
-& (Join-Path $PSScriptRoot 'pin-release-version.ps1') @args
-exit $LASTEXITCODE
--- spiker-unpin-and-purge-release.ps1
+++
@@ -1,6 +0,0 @@
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
-
-Write-Warning 'spiker-unpin-and-purge-release.ps1 is deprecated. Use unpin-release-version.ps1.'
-& (Join-Path $PSScriptRoot 'unpin-release-version.ps1') @args
-exit $LASTEXITCODE
